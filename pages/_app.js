88582247e6cf2addf8e379a497a2d5f80a22783b
Cloud <encloud@126.com>
2024-01-18 14:30:27 +0800

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 import '../styles/main.css'
 
